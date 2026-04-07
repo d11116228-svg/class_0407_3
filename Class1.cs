4975953849bcba92de1b8282f85fd18b2282ed19
d11116228-svg <d11116228@gs.takming.edu.tw>
2026-04-07 16:11:20 +0800

    Add files via upload

---
+++ Class1.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+interface IArea
+{
+    double Area();
+}
+
+public class Rectangle
+{
+    public Rectangle()
+    {
+    }
+
+    public double Area()
+    {
+        return 100.00;
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,13 @@
+﻿using System.Drawing;
+
+namespace Class_0407_3
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Rectangle r = new Rectangle();
+            Console.WriteLine(r.Area());
+        }
+    }
+}
